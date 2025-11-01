db88630dbfee931d8f8dc5054a9e7d229d15c6ef
falguni-mondal <career.falguni@gmail.com>
2025-11-01 12:47:09 +0530

    Changes for netlify testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,14 +8,14 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Your backend
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   host: true,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5000', // Your backend
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 })
