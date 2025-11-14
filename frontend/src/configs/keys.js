d5b3804fd8219904bf136e0972c282e0764d0c7c
falguni-mondal <career.falguni@gmail.com>
2025-11-15 00:52:12 +0530

    Chnages for testing netlify serverless functions

--- frontend/src/configs/keys.js
+++ frontend/src/configs/keys.js
@@ -1,2 +1,2 @@
-export const backendBaseApi = import.meta.env.VITE_BACKEND_TEST_API;
+export const backendBaseApi = import.meta.env.VITE_BACKEND_API;
 export const frontendBaseUrl = "http://localhost:5173";
